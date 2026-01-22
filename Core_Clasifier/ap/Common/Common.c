0593a4b4a57366ac3cf90aa595b234879661ca18
jaewoo01 <87527657+jaewoo01@users.noreply.github.com>
2026-01-22 18:17:10 +0900

    Delete Core_Clasifier directory

--- Core_Clasifier/ap/Common/Common.c
+++
@@ -1,7 +0,0 @@
-/*
- * Common.c
- *
- *  Created on: Jan 7, 2026
- *      Author: kccistc
- */
-#include "Common.h"
--- Core_Clasifier/ap/Listener/Listener.c
+++
@@ -1,9 +0,0 @@
-#include "Listener.h"
-
-void Listener_Init() {
-	ListenerFpgaUartInit();
-}
-
-void Listener_Excute() {
-	ListenerFpgaUartExcute();
-}
